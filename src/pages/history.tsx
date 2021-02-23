aaefeb724d55b95201f0f742a27c34599bdbbf09
ekusuy <ekusuy.omi@gmail.com>
2021-02-23 16:58:16 +0900

    history改善

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,29 +1,62 @@
 import * as React from 'react'
-import { Button } from '../components/button'
 import {
     Link,
     useHistory,
   } from 'react-router-dom'
-  import styled from 'styled-components'
-  import { Header } from '../components/header'
+import styled from 'styled-components'
+import { Header } from '../components/header'
+import {
+  getMemos,
+  MemoRecord,
+} from '../indexeddb/memos'
 
-  const HeaderArea = styled.div`
-    position: fixed;
-    right: 0;
-    top: 0;
-    left: 0;
+const { useState, useEffect } = React
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
+`
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+  padding: 0 1rem;
+`
+
+const Memo = styled.button`
+    display: block;
+    background-color: white;
+    border: 1px solid gray;
+    width: 100%;
+    padding: 1rem;
+    margin: 1rem 0;
+    text-align: left;
   `
 
-  const Wrapper = styled.div`
-    bottom: 0;
-    left: 0;
-    position: fixed;
-    right: 0;
-    top: 3rem;
-    padding: 0 1rem;
-  `
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
 
 export const History:React.FC = () => {
+  const [memos, setMemos] = useState<MemoRecord[]>([])
+
+  useEffect(() => {
+    getMemos().then(setMemos)
+  },[])
+
   const history = useHistory()
   return (
     <>
@@ -35,7 +68,12 @@ export const History:React.FC = () => {
         </Header>
       </HeaderArea>
       <Wrapper>
-        TODO: 履歴表示
+        {memos.map(memo => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
       </Wrapper>
     </>
   )
